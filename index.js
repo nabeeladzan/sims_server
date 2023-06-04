14de5f7e51925ffde1a49695d54be0420d02b0f6
Muhammad Nabeel Adzan <muhnabeeladzan@gmail.com>
2023-06-05 04:22:11 +0700

    Added Models API Suite

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const multer = require("multer");
+
 var mysql = require("mysql");
 
 // get config vars
@@ -10,6 +12,17 @@ dotenv.config();
 
 const app = express();
 const port = 3000;
+const imageUpload = multer({
+    storage: multer.diskStorage({
+        destination: "images/",
+        filename: (req, file, cb) => {
+            const randomString = generateRandomString(10);
+            const filename =
+                randomString + "." + file.originalname.split(".").pop();
+            cb(null, filename);
+        },
+    }),
+});
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -44,6 +57,18 @@ class Message {
     }
 }
 
+// Model class
+class Model {
+    constructor(id, name, price, stock, isActive, pictureUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.isActive = isActive;
+        this.pictureUrl = pictureUrl ? pictureUrl : null;
+    }
+}
+
 app.use(cors());
 
 // Configuring body parser middleware
@@ -55,7 +80,7 @@ app.get("/", (req, res) => res.send("SIMS Server"));
 
 // generate jwt token
 function generateAccessToken(key) {
-    return jwt.sign(key, process.env.TOKEN_SECRET, { expiresIn: "1800s" });
+    return jwt.sign(key, process.env.TOKEN_SECRET, { expiresIn: "14d" });
 }
 
 // Generate 10 digit key for token
@@ -116,7 +141,6 @@ app.post("/registerKey", (req, res) => {
     });
 });
 
-
 // USER MANAGEMENT
 
 // - get all users
@@ -146,7 +170,8 @@ app.post("/addUser", authenticateToken, (req, res) => {
     const pin = req.body.pin;
     const token = generateTenDigitKey();
 
-    const sql = "INSERT INTO users (first_name, last_name, pin, token) VALUES (?, ?, ?, ?)";
+    const sql =
+        "INSERT INTO users (first_name, last_name, pin, token) VALUES (?, ?, ?, ?)";
     const values = [first_name, last_name, pin, token];
 
     con.query(sql, values, (err, result) => {
@@ -162,8 +187,9 @@ app.put("/updateUser/:id", authenticateToken, (req, res) => {
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
     const pin = req.body.pin;
-    
-    const sql = "UPDATE users SET first_name = ?, last_name = ?, pin = ? WHERE id = ?";
+
+    const sql =
+        "UPDATE users SET first_name = ?, last_name = ?, pin = ? WHERE id = ?";
     const values = [first_name, last_name, pin, id];
 
     con.query(sql, values, (err, result) => {
@@ -184,17 +210,123 @@ app.delete("/deleteUser/:id", authenticateToken, (req, res) => {
     });
 });
 
+// MODEL MANAGEMENT
+
+// - get all models
+app.get("/getModels", authenticateToken, (req, res) => {
+    const sql = "SELECT * FROM models";
+
+    con.query(sql, (err, rows) => {
+        if (err) throw err;
+        // change active from 0/1 to false/true
+        for (let i = 0; i < rows.length; i++) {
+            if (rows[i].active === 0) {
+                rows[i].active = false;
+            } else {
+                rows[i].active = true;
+            }
+        }
+        res.status(200).json(rows);
+    });
+});
+
+// - get model by id
+app.get("/getModel/:id", authenticateToken, (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM models WHERE id = ?";
+    con.query(sql, [id], (err, rows) => {
+        if (err) throw err;
+        res.status(200).json(rows);
+    });
+});
+
+// - add model
+app.post("/addModel", authenticateToken, (req, res) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const stock = req.body.stock;
+    const active = req.body.active;
+
+    const sql =
+        "INSERT INTO models (name, price, stock, active) VALUES (?, ?, ?, ?)";
+    const values = [name, price, stock, active];
+
+    con.query(sql, values, (err, result) => {
+        if (err) throw err;
+        console.log("1 record inserted");
+        res.status(200).json({ message: "Model added" });
+    });
+});
+
+// - update model
+app.put("/updateModel", authenticateToken, (req, res) => {
+    const id = req.body.id;
+    const name = req.body.name;
+    const price = req.body.price;
+    const stock = req.body.stock;
+    const active = req.body.active;
+
+    const sql =
+        "UPDATE models SET name = ?, price = ?, stock = ?, active = ? WHERE id = ?";
+    const values = [name, price, stock, active, id];
+
+    con.query(sql, values, (err, result) => {
+        if (err) throw err;
+        console.log("1 record updated");
+        res.status(200).json({ message: "Model updated" });
+    });
+});
+
+// - delete model by id
+app.delete("/deleteModel", authenticateToken, (req, res) => {
+    const id = req.query.id;
+    const sql = "DELETE FROM models WHERE id = ?";
+    con.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        console.log("1 record deleted");
+        res.status(200).json({ message: "Model deleted" });
+    });
+});
+
+// image upload with authentication
+app.post(
+    "/uploadImage",
+    authenticateToken,
+    imageUpload.single("image"),
+    (req, res) => {
+        if (!req.file) {
+            return res.status(400).send("No file uploaded.");
+        }
+        res.status(200).send("File uploaded!");
+
+        // set the model picture_id to the id of the uploaded image
+        const id = req.body.id;
+        const sql = "UPDATE models SET picture_id = ? WHERE id = ?";
+        const values = [req.file.filename, id];
+
+        con.query(sql, values, (err, result) => {
+            if (err) throw err;
+            console.log("1 record updated");
+        });
+    }
+);
+
 // JWT AUTHENTICATION
 function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null) return res.sendStatus(401);
+    console.log("Authenticating token : " + token);
+
+    if (token == null) {
+        console.log("Token is empty");
+        return res.sendStatus(401);
+    }
 
     jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
         if (err) {
             console.log(err);
-            return res.sendStatus(403);
+            res.status(200).json({ message: "Token is not valid" });
         }
 
         req.user = user;
@@ -207,7 +339,9 @@ function authenticateToken(req, res, next) {
 
 // - test token
 app.get("/testToken", authenticateToken, (req, res) => {
-    res.status(200).send("valid token");
+    // return json is token is valid
+    console.log("Token is valid");
+    res.status(200).json({ message: "Token is valid" });
 });
 
 app.listen(port, () => console.log(`SIMS Server listening on port ${port}!`));
